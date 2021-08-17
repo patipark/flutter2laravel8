02b908b14b41528d73d3299b5de0ea2a6cd31a59
Patipark Sopha <iamwad@gmail.com>
2021-08-17 22:35:16 +0700

    ทำ form login เข้าสูระบบได้แล้ว แต่ยังไม่ได้ แสดงข้อมูล user และซ่อน form หรือ navigate ไป page อื่นๆ เช่น product

--- lib/screens/login.page.dart
+++ lib/screens/login.page.dart
@@ -2,6 +2,8 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter2laravel8/models/user.dart';
+import 'package:flutter2laravel8/screens/product.page.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'dart:convert' show json;
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
@@ -15,15 +17,19 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  // final _formKeys = GlobalKey<FormBuilderState>();
+  final _formKeys = GlobalKey<FormBuilderState>();
+  final textControllers = TextEditingController();
   User? _user;
+  bool isLoad = true;
 
   @override
   void initState() {
-    _login();
+    // _login();
     super.initState();
   }
 
-  void _login() async {
+  void _login(Map<String, dynamic> formData) async {
     try {
       String url = BASE_API_URL + '/login';
       print(url);
@@ -31,7 +37,8 @@ class _LoginPageState extends State<LoginPage> {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
       };
-      var body = json.encode({"email": "iamwad@gmail.com", "password": "1234"});
+      // var body = json.encode({"email": "iamwad@gmail.com", "password": "1234"});
+      var body = json.encode(formData);
       final response =
           await http.post(Uri.parse(url), headers: headers, body: body);
       // print(response.body);
@@ -45,21 +52,154 @@ class _LoginPageState extends State<LoginPage> {
         inspect(_user);
         final SharedPreferences prefs = await SharedPreferences.getInstance();
         await prefs.setString('loginData', response.body);
+        showDialogBox('Login successed!', 'เข้าสู่ระบบสำเร็จ!');
       } else {
         print('Could not post data. @ login.page._login()');
-        throw Exception('ไม่สามารถโหลดข้อมูลจาก API ได้ (Error Code:' +
-            response.statusCode.toString() +
-            ')');
+        showDialogBox('Login failed!', 'ไม่สามารถเข้าสู่ระบบได้');
       }
     } catch (e) {
       print('error on login.page._login() => $e');
     }
   }
 
+  void signIn(BuildContext context) async {
+    _formKeys.currentState!.save();
+    if (_formKeys.currentState!.validate()) {
+      print(_formKeys.currentState!.value);
+      _login(_formKeys.currentState!.value);
+    } else {
+      print("validation failed");
+      showDialogBox('Validation failed!', 'ข้อมูลไม่ครบถ้วย');
+    }
+  }
+
+  void showDialogBox(String title, String body) async {
+    return showDialog<void>(
+        context: context,
+        barrierDismissible: true,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text(title),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: <Widget>[Text(body)],
+              ),
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: Text("ปิด"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('LoginPage'),
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('เข้าสู่ระบบ Laravel8 api',
+                style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue)),
+            SizedBox(height: 20),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: FormBuilder(
+                key: _formKeys,
+                initialValue: {
+                  'email': '',
+                  'password': '',
+                },
+                // autovalidateMode: AutovalidateMode.onUserInteraction,
+                child: Column(
+                  children: <Widget>[
+                    FormBuilderTextField(
+                      name: "email",
+                      maxLines: 1,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                          labelText: "Email",
+                          labelStyle: TextStyle(color: Colors.black87),
+                          fillColor: Colors.white,
+                          filled: true,
+                          border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10))),
+                          errorStyle: TextStyle(color: Colors.white)),
+                      validator: FormBuilderValidators.compose([
+                        FormBuilderValidators.required(context),
+                        FormBuilderValidators.email(context),
+                      ]),
+                    ),
+                    SizedBox(height: 20),
+                    FormBuilderTextField(
+                      name: "password",
+                      maxLines: 1,
+                      obscureText: true,
+                      keyboardType: TextInputType.visiblePassword,
+                      decoration: InputDecoration(
+                          labelText: "Password",
+                          labelStyle: TextStyle(color: Colors.black87),
+                          fillColor: Colors.white,
+                          filled: true,
+                          border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10))),
+                          errorStyle: TextStyle(color: Colors.white)),
+                      validator: FormBuilderValidators.compose([
+                        FormBuilderValidators.required(context),
+                        FormBuilderValidators.min(context, 4),
+                      ]),
+                    ),
+                    SizedBox(height: 20),
+                    SizedBox(
+                      width: size.width * 1,
+                      height: 72,
+                      child: MaterialButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12.0)),
+                        color: Colors.blue,
+                        textColor: Colors.white,
+                        child: Stack(
+                          children: [
+                            Positioned(
+                              left: 0.0,
+                              child: !isLoad
+                                  ? Container(
+                                      width: 20.0,
+                                      height: 20.0,
+                                      child: CircularProgressIndicator())
+                                  : SizedBox.shrink(),
+                            ),
+                            Center(
+                              child: Text('Log In',
+                                  style: TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white)),
+                            ),
+                          ],
+                        ),
+                        onPressed: () => signIn(context),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
