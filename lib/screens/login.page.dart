9c6ae9bebf7b7671cb5c6750b2449376ce47d7a7
Patipark Sopha <iamwad@gmail.com>
2021-08-17 17:27:10 +0700

    เมื่อ login เสร็จให้เก็บค่าใน shared preference และใส่ข้อมูลเข้าไปใน model User และ inspect ดู ได้ข้อมูลแล้ว

--- lib/screens/login.page.dart
+++ lib/screens/login.page.dart
@@ -1,6 +1,10 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter2laravel8/models/user.dart';
 import 'dart:convert' show json;
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 import '../constant.dart';
 
 class LoginPage extends StatefulWidget {
@@ -11,6 +15,8 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  User? _user;
+
   @override
   void initState() {
     _login();
@@ -28,10 +34,17 @@ class _LoginPageState extends State<LoginPage> {
       var body = json.encode({"email": "iamwad@gmail.com", "password": "1234"});
       final response =
           await http.post(Uri.parse(url), headers: headers, body: body);
-      print(response.body);
-      if (response.statusCode == 200) {
+      // print(response.body);
+      if (response.statusCode == 200 || response.statusCode == 201) {
         var parsed = json.decode(response.body);
-        print(parsed);
+        // print(parsed);
+        // var user = User.fromJson(parsed['user']);
+        setState(() {
+          _user = User.fromJson(parsed['user']);
+        });
+        inspect(_user);
+        final SharedPreferences prefs = await SharedPreferences.getInstance();
+        await prefs.setString('loginData', response.body);
       } else {
         print('Could not post data. @ login.page._login()');
         throw Exception('ไม่สามารถโหลดข้อมูลจาก API ได้ (Error Code:' +
